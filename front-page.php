26225114845082d1798f4988f31046f3dfbbd812
Tyler <tiller1010@gmail.com>
2019-02-19 16:27:39 -0500

    added site title and tagline to front-page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,8 @@
 <?php get_header();?>
 
 <div>
-	<h1><?php the_title();?></h1>
+	<h1><?php bloginfo('name');?></h1>
+	<h1><?php bloginfo('description');?></h1>
 
 	<?php if (have_posts()) : while(have_posts()) : the_post();?>
 
